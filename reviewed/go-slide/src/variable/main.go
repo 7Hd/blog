fab90bd530da94d8ed271c3fddbb46fcb844e45b
7Hd <kevin1200x@gmail.com>
2018-06-25 22:31:45 +0800

    update go slide

---
+++ reviewed/go-slide/src/variable/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Declare the variable “name”.
+	var name string // HL
+
+	// Assign a string value to it.
+	name = "Michael"
+
+	hello := "Hello" // HL
+
+	// Call the variable.
+	fmt.Println(hello, name)
+}
