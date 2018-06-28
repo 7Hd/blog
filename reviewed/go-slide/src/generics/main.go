1fe0dfd0fec4f0948f942b8819d5c7e29eaad5ee
PHL <kevin1200x@gmail.com>
2018-06-28 15:25:59 +0800

    update generics

--- reviewed/go-slide/src/generics/main.go
+++ reviewed/go-slide/src/generics/main.go
@@ -1,3 +1,25 @@
 package main
 
+import "fmt"
+
+// IFN 定義需實作的 func
+type IFN interface {
+	Get() int
+	Set(int)
+}
+
+func genericsFN(fn IFN) {
+	fn.Set(1)
+	fn.Get()
+}
+
+func any(v interface{}) {
+	switch val := v.(type) {
+	case string:
+		fmt.Println("v (string)", val)
+	case int:
+		fmt.Println("v (int)", val)
+	}
+}
+
 func main() {}
